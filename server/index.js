c6005b3e7100a7a2d2886228241ba795c58ea021
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-15 13:43:11 +0100

    added all queries needed for client and project

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 require('dotenv').config()
 const { graphqlHTTP } = require('express-graphql')
+const colors = require('colors')
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 
 const app = express()
+
+connectDB()
 
 app.use('/graphql', graphqlHTTP({
     schema,
